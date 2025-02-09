03e403d3595f29b53456a0b0b0b64cbc262e544f
08-ayush <aayushchauhan008@gmail.com>
2025-02-10 00:16:34 +0530

    login<endpoint>

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from '../screen/Home';
+import Login from '../screen/Login';
+import Register from '../screen/Register';
+// import Project from '../screen/Project';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<div>login</div>} />
+        <Route
+          path="/"
+          element=
+          
+          {
+            // <UserAuth>
+              <Home />
+            // </UserAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route
+          path="/project"
+          element={
+            <UserAuth>
+              <Project />
+            </UserAuth>
+          }
+        /> */}
       </Routes>
     </BrowserRouter>
   );
